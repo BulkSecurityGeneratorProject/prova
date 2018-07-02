f3df3086a769a611cf242b45bf03c3141cd556b3
FrancescaStanzial <francesca.stanzial@ppro.it>
2018-07-02 17:52:43 +0200

    modifica francesca 1

--- src/main/java/it/ppro/prova/ProvaApp.java
+++ src/main/java/it/ppro/prova/ProvaApp.java
@@ -56,6 +56,7 @@ public class ProvaApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+    	//francesca locale
         SpringApplication app = new SpringApplication(ProvaApp.class);
         DefaultProfileUtil.addDefaultProfile(app);
         Environment env = app.run(args).getEnvironment();
